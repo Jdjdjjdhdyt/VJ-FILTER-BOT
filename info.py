7d278ba472dbb72edd77e1f28db3e48f3dff415f
Jdjdjjdhdyt <ilashadam1@gmail.com>
2024-06-09 19:53:29 +0400

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = environ.get('SESSION', 'TechVJBot')
+SESSION = environ.get('SESSION', '')
 API_ID = int(environ.get('API_ID', '22182189'))
 API_HASH = environ.get('API_HASH', '5e7c4088f8e23d0ab61e29ae11960bf5')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
